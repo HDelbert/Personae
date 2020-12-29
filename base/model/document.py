f1295197e0530030f076bd1fe6613dc157618377
幻翎 <jade.hm@alibaba-inc.com>
2020-12-30 00:27:08 +0800

    update double color ball

--- base/model/document.py
+++ base/model/document.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 from mongoengine import Document
-from mongoengine.fields import StringField, FloatField, DateTimeField
+from mongoengine.fields import StringField, FloatField, DateTimeField, IntField
 
 
 class Stock(Document):
@@ -118,3 +118,55 @@ class Future(Document):
     @classmethod
     def exist_in_db(cls, code):
         return True if cls.objects(code=code)[:1].count() else False
+
+
+class ColorBall(Document):
+    # 期数
+    phase = StringField(required=True)
+    # 开奖日期
+    date = DateTimeField(required=True)
+    # 红1
+    red1 = IntField()
+    # 红2
+    red2 = IntField()
+    # 红3
+    red3 = IntField()
+    # 红4
+    red4 = IntField()
+    # 红5
+    red5 = IntField()
+    # 红6
+    red6 = IntField()
+    # 蓝
+    blue = IntField()
+
+    meta = {
+        'indexes': [
+            'phase',
+            'date',
+            ('phase', 'date')
+        ]
+    }
+
+    def save_if_need(self):
+        return self.save() if len(self.__class__.objects(phase=self.phase, date=self.date)) < 1 else None
+
+    def to_state(self):
+        ball_dic = self.to_mongo()
+        ball_dic.pop('_id')
+        ball_dic.pop('phase')
+        ball_dic.pop('date')
+        return ball_dic.values()
+
+    def to_dic(self):
+        ball_dic = self.to_mongo()
+        ball_dic.pop('_id')
+        return ball_dic.values()
+
+    @classmethod
+    def get_ball_data(cls, start, end):
+        return cls.objects(date__gte=start, date__lte=end).order_by('phase')
+
+    @classmethod
+    def exist_in_db(cls, phase):
+        return True if cls.objects(phase=phase)[:1].count() else False
--- helper/args_parser.py
+++ helper/args_parser.py
@@ -2,7 +2,7 @@
 
 import argparse
 
-stock_codes = ["600031", "601328", "601998", "601398"]
+stock_codes = ["600031", "601988"]
 future_codes = ["AU88", "RB88", "CU88", "AL88"]
 
 stock_spider_parser = argparse.ArgumentParser()
@@ -24,3 +24,7 @@ model_launcher_parser.add_argument("--market", default="stock")
 model_launcher_parser.add_argument("--episode", default=500, type=int)
 model_launcher_parser.add_argument("--train_steps", default=100000, type=int)
 model_launcher_parser.add_argument("--training_data_ratio", default=0.8, type=float)
+
+ball_spider_parser = argparse.ArgumentParser()
+ball_spider_parser.add_argument("-s", "--start", default='20000')
+ball_spider_parser.add_argument('-e', "--end", default='20133')
